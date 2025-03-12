4c38696b5c16e83e25c6fd7128002a3ab340ecbb
tardes <Krycoow@gmail.com>
2025-03-12 19:55:10 +0100

    commit v1
    Signed-off-by: tardes <Krycoow@gmail.com>

--- app/src/main/java/com/krycoow/menudemisappsv3/MenuActivity.kt
+++ app/src/main/java/com/krycoow/menudemisappsv3/MenuActivity.kt
@@ -3,23 +3,25 @@ package com.krycoow.menudemisappsv3
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-
 
 class MenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_menu)
-        val btnCalcuImc = findViewById<Button>(R.id.btnCalcuImc)
-        btnCalcuImc.setOnClickListener {  }
+
+        // Encontrar el botón por su ID
+        val btnSaludApp = findViewById<Button>(R.id.btnSaludApp)
+
+        // Configurar el listener para el botón
+        btnSaludApp.setOnClickListener {
+            navigateToSaludApp()
+        }
     }
 
-    fun navigateToCalcuIMC(){
-        val intent = Intent(this)
-
-
+    // Función para navegar a la siguiente actividad
+    private fun navigateToSaludApp() {
+        val intent = Intent(this, FirstAppActivity::class.java)
+        startActivity(intent)
     }
-
 }
