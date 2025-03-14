77b6ab3b050693769212bc64ddad77030d740c2d
tardes <Krycoow@gmail.com>
2025-03-14 20:00:49 +0100

    commit v2
    Signed-off-by: tardes <Krycoow@gmail.com>

--- app/src/main/java/com/krycoow/menudemisappsv3/imccalculator/ImcCalculatorActivity.kt
+++ app/src/main/java/com/krycoow/menudemisappsv3/imccalculator/ImcCalculatorActivity.kt
@@ -1,23 +1,25 @@
 package com.krycoow.menudemisappsv3.imccalculator
 
+import android.icu.text.DecimalFormat
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.cardview.widget.CardView
 import androidx.core.content.ContextCompat
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.slider.RangeSlider
 import com.krycoow.menudemisappsv3.R
 
 class ImcCalculatorActivity : AppCompatActivity() {
 
-    private var isMaleSelected:Boolean = true
-    private var isFemaleSelected:Boolean = true
 
+    private var isMaleSelected:Boolean = true
+    private var isFemaleSelected:Boolean = false
 
     private lateinit var viewMale:CardView
     private lateinit var viewFemale:CardView
-
+    private lateinit var tvHeight:TextView
+    private lateinit var rsHeight:RangeSlider
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,47 +31,46 @@ class ImcCalculatorActivity : AppCompatActivity() {
 
     }
 
-
-
-
-    private fun initComponents() {
-
-        viewMale = findViewById(R.id.viewMale)
-        viewFemale = findViewById(R.id.viewFemale)
-
-    }
-
     private fun initListeners() {
         viewMale.setOnClickListener {
             setGenderColor()
             changeGender()
+
         }
 
         viewFemale.setOnClickListener {
             setGenderColor()
             changeGender()
         }
+        rsHeight.addOnChangeListener { _,value,_->
+            val df = DecimalFormat("#.##")
+            val result = df.format(value)
+            tvHeight.text = "$result cm"
+
+        }
 
     }
+
 
     private fun changeGender(){
-
-       isMaleSelected = !isMaleSelected
+        isMaleSelected = !isMaleSelected
         isFemaleSelected = !isFemaleSelected
-
     }
 
-    private fun setGenderColor(){
 
+
+    private fun setGenderColor(){
         viewMale.setCardBackgroundColor(getBackgroundColor(isMaleSelected))
         viewFemale.setCardBackgroundColor(getBackgroundColor(isFemaleSelected))
 
-
     }
 
-    private fun getBackgroundColor(isSelectedComponent:Boolean) : Int{
 
-        val colorReference = if (isSelectedComponent){
+
+    private fun getBackgroundColor(isSelectedComponet:Boolean):Int{
+
+
+        val colorReference = if(isSelectedComponet){
             R.color.background_component_selected
         }else{
             R.color.background_component
@@ -79,7 +80,14 @@ class ImcCalculatorActivity : AppCompatActivity() {
 
     }
 
-    private fun initUI() {
+
+    private fun initComponents(){
+        viewMale = findViewById(R.id.viewMale)
+        viewFemale = findViewById(R.id.viewFemale)
+        tvHeight = findViewById(R.id.tvHeight)
+        rsHeight = findViewById(R.id.rsHeight )
+    }
+    private fun initUI(){
 
         setGenderColor()
 
